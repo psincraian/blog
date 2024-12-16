668dac92965807e585fa1e0b3444dc32b3257157
Petru Rares Sincraian <petrurs@amazon.es>
2024-12-16 15:07:50 +0100

    feat: update career

--- app/components/career-timeline.tsx
+++ app/components/career-timeline.tsx
@@ -1,73 +1,113 @@
 import Image from 'next/image'
 
 interface TimelineItem {
-  company: string
-  logo: string
-  position: string
-  duration: string
-  achievements: string[]
+    company: string
+    logo: string
+    position: string
+    duration: string
+    achievements: string[]
 }
 
 const timelineData: TimelineItem[] = [
-  {
-    company: 'Tech Corp',
-    logo: '/tech-corp-logo.png',
-    position: 'Senior Developer',
-    duration: '2020 - Present',
-    achievements: [
-      'Led a team of 5 developers in creating a new product feature',
-      'Improved application performance by 40%',
-      'Implemented CI/CD pipeline, reducing deployment time by 60%',
-    ],
-  },
-  {
-    company: 'Startup Inc',
-    logo: '/startup-inc-logo.png',
-    position: 'Full Stack Developer',
-    duration: '2018 - 2020',
-    achievements: [
-      'Developed and launched the company\'s flagship product',
-      'Increased user engagement by 25% through UI/UX improvements',
-      'Mentored junior developers and interns',
-    ],
-  },
-  // Add more timeline items as needed
+    {
+        company: 'Amazon',
+        logo: '/career/amazon.jpeg',
+        position: 'Software Engineer',
+        duration: '2022 - Present',
+        achievements: [
+            'Led the deprecation and modernization of a 20+ year-old system, collaborating with cross-functional tech teams to streamline processes. Stack: Java, RDS, Event-Driven architecture, and other AWS services',
+            'Designed and implemented a modular monolith architecture, simplifying system complexity and reducing maintenance overhead. Stack: Java, Modular Monolith, DDD, RDS, SQS, and other AWS',
+            'Led the analysis and development of a critical internal payroll tool, successfully deployed across 63+ countries, addressing each country specific requirements. Stack: Java, DynamoDB, and other AWS services',
+        ],
+    },
+    {
+        company: 'Glovo',
+        logo: '/career/glovo.jpeg',
+        position: 'Sr. Software Engineer',
+        duration: '2022 - 2022',
+        achievements: [
+            'Built a Python web service to automate the discovery and creation of data products, reducing manual effort by 50%. Stack: Python, Flask, AirFlow, SnowFlake, and AWS',
+        ],
+    },
+    {
+        company: 'Ocado',
+        logo: '/career/ocado.png',
+        position: 'Sr. Software Engineer',
+        duration: '2019 - 2022',
+        achievements: [
+            'Led the development of the first customer segmentation service, enabling personalized experiences for millions of customers. Stack: Java, Spring, DynamoDB, Event-Driven architecture, BigQuery, and other AWS services',
+            'Wrote guidelines on input validation for different languages and alphabets, used by all tech teams to support international customers, including a new customer from Japan.',
+            'Implemented an internal search to access internal tools faster. It has been used by 35% of internal employees. Stack: React and CloufFront',
+        ],
+    },
+    {
+        company: 'Prope',
+        logo: '/career/prope.jpeg',
+        position: 'Founder',
+        duration: '2018 - 2020',
+        achievements: [
+            'Developed and launched a website that connected 20+ shops with 1000+ customers, increasing local business visibility.',
+            'Built the backend using Python and Flask, hosted on DigitalOcean, and developed the frontend and mobile apps with React and React Native.',
+        ],
+    },
+    {
+        company: 'Ulabox',
+        logo: '/career/ulabox.jpeg',
+        position: 'Software Engineer',
+        duration: '2017 - 2019',
+        achievements: [
+            'Built and deployed the first recommendation system, increasing the average order value by 2% for customers. Stack: Python, Flask, BigQuery, Pandas',
+            'Developed a Delivery Windows module, providing users with flexible delivery options based on complex business constraints. Stack: PHP, Symfony, MariaDB',
+            'Built a Purchasing System for easier supplier interactions, optimizing internal processes. Stack: PHP; Symfony, MariaDB',
+        ],
+    },
+    {
+        company: 'Google Summer of Code',
+        logo: '/career/gsoc.png',
+        position: 'Intern Software Engineer',
+        duration: '2016 - 2016',
+        achievements: [
+            'Contributed to the FFmpeg project, improving testing accuracy and system stability. Stack: C, Python, Docker',
+        ],
+    }
+    // Add more timeline items as needed
 ]
 
 export const CareerTimeline = () => {
-  return (
-    <section className="mb-16">
-      <h2 className="text-3xl font-bold mb-8 text-center">Career Timeline</h2>
-      <div className="relative">
-        {timelineData.map((item, index) => (
-          <div key={index} className="mb-8 flex justify-between items-center">
-            <div className="w-1/2 pr-8 text-right">
-              <h3 className="text-xl font-semibold">{item.company}</h3>
-              <p className="text-gray-600">{item.position}</p>
-              <p className="text-sm text-gray-500">{item.duration}</p>
-            </div>
-            <div className="w-12 h-12 bg-blue-600 rounded-full flex items-center justify-center">
-              <Image
-                src={item.logo}
-                alt={`${item.company} logo`}
-                width={32}
-                height={32}
-                className="rounded-full"
-              />
-            </div>
-            <div className="w-1/2 pl-8">
-              <ul className="list-disc pl-4">
-                {item.achievements.map((achievement, i) => (
-                  <li key={i} className="mb-2 text-sm">
-                    {achievement}
-                  </li>
+    return (
+        <section className="mb-16">
+            <h2 className="text-3xl font-bold mb-8 text-center">Career Timeline</h2>
+            <div className="relative">
+                {timelineData.map((item, index) => (
+                    <div key={index} className="mb-8 flex justify-between items-center">
+                        <div className="w-1/2 pr-8 text-right">
+                            <h3 className="text-xl font-semibold">{item.company}</h3>
+                            <p className="text-gray-600">{item.position}</p>
+                            <p className="text-sm text-gray-500">{item.duration}</p>
+                        </div>
+                        <div
+                            className="z-10 backdrop-blur p-1 border-gray-600 border-2 rounded-full flex items-center justify-center">
+                            <Image
+                                src={item.logo}
+                                alt={`${item.company} logo`}
+                                width={64}
+                                height={64}
+                                className="rounded-full"
+                            />
+                        </div>
+                        <div className="w-1/2 pl-8">
+                            <ul className="list-disc pl-4">
+                                {item.achievements.map((achievement, i) => (
+                                    <li key={i} className="mb-2 text-sm">
+                                        {achievement}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
                 ))}
-              </ul>
+                <div className="absolute top-0 bottom-0 left-1/2 w-1 bg-gray-200 transform -translate-x-1/2"></div>
             </div>
-          </div>
-        ))}
-        <div className="absolute top-0 bottom-0 left-1/2 w-1 bg-blue-200 transform -translate-x-1/2"></div>
-      </div>
-    </section>
-  )
+        </section>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,17 @@ export default function Home() {
     <div className="container mx-auto px-4 py-8">
       <section className="text-center mb-16">
         <Image
-          src="/me.jpeg"
+          src="/me.png"
           alt="Petru Rares Sincraian"
           width={200}
           height={200}
           className="rounded-full mx-auto mb-4"
         />
-        <h1 className="text-4xl font-bold mb-2">Your Name</h1>
-        <p className="text-xl text-gray-600 mb-4">Your Tagline Here</p>
+        <h1 className="text-4xl font-bold mb-2">Petru Rares Sincraian</h1>
+        <p className="text-xl text-gray-600 mb-4">Software Engineer</p>
         <p className="max-w-2xl mx-auto text-gray-700 mb-6">
-          Your brief, engaging bio goes here. It should be around 150-200 words,
-          highlighting your key skills, experiences, and what makes you unique.
-          Make it personal and engaging to capture the reader&apos;s attention.
+          I’m an optimistic and hardworking software engineer who firmly believes success is 90% work and 10% luck. <br/>
+          I love building cool stuff and delivering it to customers—faster, better, and with a little style. While I proudly call myself a backend engineer, I’m not afraid to break the front-end or infrastructure when the job demands it.
         </p>
         <div className="flex justify-center space-x-4">
           <Link
